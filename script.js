c33e51a88d809c8eeb79c59559996e478d5b64f0
Filip Todorovic <ftodorovic@hotmail.com>
2021-11-26 14:11:33 +0100

    Project Cafe Barista

---
+++ script.js
@@ -0,0 +1,15 @@
+// 'use strict';
+//
+// const imgStory = document.querySelector('.img--story');
+// const clickedImg = function () {
+//   imgStory.classList.add('active--img');
+// };
+//
+// const removeActiveImg = function(){
+//   imgStory.classList.remove('active--img');
+// };
+//
+// imgStory.addEventListener('click',clickedImg);
+//
+// const overlay = document.querySelector('.overlay');
+// overlay.addEventListener('click',removeActiveImg);
